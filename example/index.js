8375747bf706212561871d239e5d71a72d891321
jeffreywescott <jeffrey@learnersguild.org>
2016-11-23 13:39:56 -0800

    change example to assume babel-runtime environment

--- example/index.js
+++ example/index.js
@@ -1,7 +1,3 @@
-/* eslint-disable import/no-unassigned-import */
-require('babel-core/register')
-require('babel-polyfill')
-
 if (!module.parent) {
   require('./example')()
     .catch(err => console.error(err))
